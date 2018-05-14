8f01a04a6d63192f5eb9102a999c7221508f61db
Lukas Lücke <lukas@luecke.me>
2018-05-14 10:01:14 +0200

    feat(di): register class as service

--- DependencyInjection/Compiler/TableFactoryPass.php
+++ DependencyInjection/Compiler/TableFactoryPass.php
@@ -70,7 +70,7 @@ class TableFactoryPass implements CompilerPassInterface
 
         // add remaining extensions to table factory
         foreach ($container->findTaggedServiceIds('table.extension') as $id => $tags) {
-            $container->getDefinition('whatwedo_table.factory.table')->addMethodCall('addExtension', [new Reference($id)]);
+            $container->getDefinition('whatwedo\TableBundle\Factory\TableFactory')->addMethodCall('addExtension', [new Reference($id)]);
         }
 
     }
