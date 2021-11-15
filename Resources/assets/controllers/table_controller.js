445d73632009526b8bb7572d2009c29821dfbd6f
Marc Wieland <marc@whatwedo.ch>
2021-11-15 17:13:43 +0100

    feat: add correct targets for stimulus controller

--- Resources/assets/controllers/table_controller.js
+++ Resources/assets/controllers/table_controller.js
@@ -2,9 +2,11 @@ import { Controller } from 'stimulus';
 import * as StickyThead from 'stickythead'
 
 export default class extends Controller {
+    static targets = ['table']
+
     connect() {
-        StickyThead.apply([this.element], {
-            scrollableArea: document.querySelector('[data-whatwedo-crud="content"]')
+        StickyThead.apply([this.tableTarget], {
+            scrollableArea: this.element
         });
     }
 }
