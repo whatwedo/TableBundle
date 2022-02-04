5adcbc36dfd0991bb88fdac370cd24bafb8cfa45
Maurizio Monticelli <rugbymauri@gmail.com>
2022-02-04 18:00:23 +0100

    test: get test working

--- tests/WiringTest.php
+++ tests/WiringTest.php
@@ -5,13 +5,14 @@ declare(strict_types=1);
 namespace whatwedo\TableBundle\Tests;
 
 use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
+use whatwedo\TableBundle\Factory\TableFactory;
 
 class WiringTest extends KernelTestCase
 {
     public function testServiceWiring()
     {
         foreach ([
-            DefinitionManager::class,
+            TableFactory::class,
         ] as $serviceClass) {
             $this->assertInstanceOf(
                 $serviceClass,
