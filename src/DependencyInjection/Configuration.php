5648ed904dbd4493e31eb3c4260627f0d213a4a1
Sandro Rüfenacht <sandro@whatwedo.ch>
2023-11-14 17:15:08 +0100

    fix: properly implement variable and code cleanup

--- src/DependencyInjection/Configuration.php
+++ src/DependencyInjection/Configuration.php
@@ -6,16 +6,26 @@ namespace araise\TableBundle\DependencyInjection;
 
 use Symfony\Component\Config\Definition\Builder\TreeBuilder;
 use Symfony\Component\Config\Definition\ConfigurationInterface;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
 
 class Configuration implements ConfigurationInterface
 {
+    public function __construct(
+        protected ContainerBuilder $containerBuilder
+    ) {
+    }
+
     public function getConfigTreeBuilder(): TreeBuilder
     {
         $treeBuilder = new TreeBuilder('araise_table');
 
+        $coreConfig = $this->containerBuilder->getParameter('araise_core.enable_turbo');
+
         $treeBuilder->getRootNode()
             ->children()
-            ->booleanNode('enable_turbo')->defaultFalse()->end();
+            ->booleanNode('enable_turbo')
+            ->defaultValue($coreConfig)
+            ->end();
 
         return $treeBuilder;
     }
--- src/DependencyInjection/araiseTableExtension.php
+++ src/DependencyInjection/araiseTableExtension.php
@@ -6,50 +6,25 @@ namespace araise\TableBundle\DependencyInjection;
 
 use Symfony\Component\Config\FileLocator;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
-use Symfony\Component\DependencyInjection\Extension\PrependExtensionInterface;
 use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
 use Symfony\Component\HttpKernel\DependencyInjection\Extension;
 
-class araiseTableExtension extends Extension implements PrependExtensionInterface
+class araiseTableExtension extends Extension
 {
+    public function getConfiguration(array $config, ContainerBuilder $container)
+    {
+        return new Configuration($container);
+    }
+
     public function load(array $configs, ContainerBuilder $container): void
     {
-        $configuration = new Configuration();
+        $configuration = new Configuration($container);
         $config = $this->processConfiguration($configuration, $configs);
 
+        $container->setParameter('araise_table.enable_turbo', $config['enable_turbo']);
         $container->setParameter('araise.enable_turbo', $config['enable_turbo']);
 
         $loader = new YamlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
         $loader->load('services.yml');
-    }
-
-    public function prepend(ContainerBuilder $container): void
-    {
-        if (!$container->hasExtension('araise_core')) {
-            return;
-        }
-        $configs = $container->getExtensionConfig($this->getAlias());
-
-        foreach (array_reverse($configs) as $config) {
-            if (isset($config['enable_turbo'])) {
-                $container->prependExtensionConfig('araise_core', [
-                    'enable_turbo' => $config['enable_turbo'],
-                ]);
-            }
-        }
-
-        if (! $container->hasExtension('doctrine_migrations')) {
-            return;
-        }
-
-        $doctrineConfig = $container->getExtensionConfig('doctrine_migrations');
-        $container->prependExtensionConfig('doctrine_migrations', [
-            'migrations_paths' => array_merge(
-                array_pop($doctrineConfig)['migrations_paths'] ?? [],
-                [
-                    'araise\TableBundle\Migrations' => '@araiseTableBundle/Migrations',
-                ]
-            ),
-        ]);
     }
 }
